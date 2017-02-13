c7d9fe1d5963df0db18fb3fd0ddcbba4df3531e2
moov <moov@moov-PC>
2017-02-13 22:00:04 +0900

    s

---
+++ realEstate/src/main/java/com/example/RealEstateApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RealEstateApplication.class, args);
+    }
+}
